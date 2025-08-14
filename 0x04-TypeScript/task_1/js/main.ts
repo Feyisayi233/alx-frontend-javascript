cf8591c79e4fddbc3b2749e5d034e1edaaa381aa
Feyisayo <feyisayo233@gmail.com>
2025-08-14 08:23:51 +0100

    task 1 file

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -1,34 +1,18 @@
 interface Teacher {
-  readonly firstName: string; // read-only
-  readonly lastName: string; // read-only
-  fullTimeEmployee: boolean; // mandatory
-  location: string; // mandatory
-  yearsOfExperience?: number; // optional
-  [key: string]: any; // allow extra attributes
+  readonly firstName: string;
+  readonly lastName: string;
+  fullTimeEmployee: boolean;
+  yearsOfExperience?: number;
+  location: string;
+  [key: string]: any;
 }
 
-interface Directors extends Teacher {
-  numberOfReports: number; // mandatory for Directors
-}
-
-// Example Teacher object
 const teacher3: Teacher = {
   firstName: "John",
-  lastName: "Doe",
   fullTimeEmployee: false,
+  lastName: "Doe",
   location: "London",
   contract: false,
 };
 
 console.log(teacher3);
-
-// Example Directors object
-const director1: Directors = {
-  firstName: "John",
-  lastName: "Doe",
-  location: "London",
-  fullTimeEmployee: true,
-  numberOfReports: 17,
-};
-
-console.log(director1);
