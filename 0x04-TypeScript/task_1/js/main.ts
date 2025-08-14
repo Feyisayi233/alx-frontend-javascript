9449f7a1e51e1b3fb6378f3aee98f4aecc4854e1
Feyisayo <feyisayo233@gmail.com>
2025-08-14 07:54:18 +0100

    update

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -1,19 +1,17 @@
-// task_1/js/main.ts
-
 interface Teacher {
   readonly firstName: string;
   readonly lastName: string;
   fullTimeEmployee: boolean;
   yearsOfExperience?: number;
   location: string;
-  [key: string]: any; // allows extra properties like 'contract'
+  [key: string]: any;
 }
 
 const teacher3: Teacher = {
-  firstName: "John",
-  lastName: "Doe",
+  firstName: 'John',
+  lastName: 'Doe',
   fullTimeEmployee: false,
-  location: "London",
+  location: 'London',
   contract: false,
 };
 
